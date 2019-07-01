02a58db09f120db2ddfad8ffd5f59e646e8de5bd
“Sam <sammans@Sams-MBP.fios-router.home>
2019-07-01 08:16:36 -0400

    Setting up serverJS file with code to access database once it's available. Adding model directory to hold models for database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,30 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
-app.get('/', (req, res) => { res.send('Hello World') });
+// app.get('/', (req, res) => { res.send('Hello World') });
+//Sends 'Hello world as a response for testing to see if the server request works
+
+const router = express.Router();
+
+app.use(cors());//Middleware for using resources located outside the server
+
+// app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json);
+
+// mongoose.connect('');
+
+// const connection = mongoose.connection;
+
+// connection.once('open', () => {
+//     console.log('MongoDB database connection established successfully!')
+// })
+//Event listener for the database open event
+
+app.use('/', router);
 
 const port = 4000;//Port number being used
 app.listen(port, () => console.log(`Express server running on port ${port}`));
+
