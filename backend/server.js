a5deb38cd41677d7a052414ad3f2322f4d522a66
“Sam <sammans@Sams-MBP.fios-router.home>
2019-07-01 15:47:30 -0400

    Setting up the different routes and logic for viewing, adding, and deleting issues

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
+import Issue from './models/Issue';
+
 const app = express();
 // app.get('/', (req, res) => { res.send('Hello World') });
 //Sends 'Hello world as a response for testing to see if the server request works
@@ -22,6 +24,68 @@ app.use(bodyParser.json);
 //     console.log('MongoDB database connection established successfully!')
 // })
 //Event listener for the database open event
+
+router.route('/issues').get((req, res) => {
+    Issue.find((err, issues) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.json();
+        }
+    })
+});//HTTP GET for a list of issues in JSON format
+
+router.route('/issues/:id').get((req, res) => {
+    Issue.findById(req.params.id, (err, issue) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(issue);
+        }
+    })
+});//colon id defines an url parameter. This can be used to retrieve a specific issue.
+
+router.route('/issues/add').post((req, res) => {
+    let issue = new Issue(req.body);
+    issue.save()
+        .then(issue => {
+            res.status(200).json({'issue': 'Added successfully'})
+        }).catch(err => {
+            res.status(400).send('Failed to create a new record');
+        });
+
+})
+
+router.route('/issues/update/:id').post((req, res) => {
+    Issue.findById(req.params.id, (err, issue) => {
+        if (!issue) {
+            return next(new Error('Could not load document'));
+        } else {
+            issue.title = req.body.title;
+            issue.responsible = req.body.responsible;
+            issue.description = req.body.description;
+            issue.severity = req.body.severity;
+            issue.status = req.body.status;
+
+            issue.save().then(issue => {
+                res.json('Update done');
+            }).catch(err => {
+                res.status(400).send('Update failed');
+            });
+        }
+    });
+});
+
+router.route('/issues/delete.:id').get((req, res) => {
+    Issue.findByIdAndRemove({_id: req.params.id}, (err, issue) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json('Removed successfuly!');
+        }
+    })
+})
 
 app.use('/', router);
 
